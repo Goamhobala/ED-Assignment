f2860687270113211f1bb8d0c8a1f5637c7d69ad
Goamhobala <yehbebe0112@gmail.com>
2024-09-07 21:31:48 +0200

    added qqplot for before clt

--- speed-comparison-master/data/data.R
+++ speed-comparison-master/data/data.R
@@ -2,15 +2,20 @@
 library(tidyverse)
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
-ishango <- read.csv("ishango/heavy/ishango_mean.csv")
-ishango.c <- ishango$c
-ishango.cpp <- ishango$cpp
-ishango.java <- ishango$java
-ishango.python <- ishango$python
-ishango.ruby <- ishango$ruby
-ishango.r <- ishango$r
 
-ishango_long <- pivot_longer(ishango, cols=everything(), names_to="language", values_to="runtime")
+# Before CLT
+ishango.before <- read.csv("ishango/pc2/pc2.csv")
+ishango.before.long <- pivot_longer(ishango.before, cols=everything(), names_to="language", values_to="runtime")
+ggplot(ishango.before.long, aes(sample=(runtime)))+
+  stat_qq(col="green")+
+  stat_qq_line(col="blue")+
+  facet_wrap(~language, scales="free")+
+  labs(x="quantiles", y="ms")
+
+# After CLT
+ishango.after <- read.csv("ishango/heavy/ishango_mean.csv")
+
+ishango.after.long <- pivot_longer(ishango.after, cols=everything(), names_to="language", values_to="runtime")
 ggplot(ishango_long, aes(sample=(runtime)))+
   stat_qq(col="coral")+
   stat_qq_line(col="blue")+
