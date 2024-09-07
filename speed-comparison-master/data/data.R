120865712684e14306c8089fdb27c560e37ea5ec
Jing Yeh <yhxjin001@>
2024-09-07 15:13:42 +0200

    added data for ishango

--- speed-comparison-master/data/data.R
+++ speed-comparison-master/data/data.R
@@ -1,10 +1,11 @@
 # analysis.r
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
-ishango <- read.csv("ishango/pc2/pc2.csv")
+ishango <- read.csv("ishango/pc1/ishango_mean.csv")
 ishango.c <- ishango$c
 ishango.cpp <- ishango$cpp
 ishango.java <- ishango$java
 ishango.python <- ishango$python
 ishango.ruby <- ishango$ruby
 ishango.r <- ishango$r
+test <- read.csv("ishango/pc2/pc2.csv")
