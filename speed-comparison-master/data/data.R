84d8bdb4b66dbd9906ca3bff263860fd1eab03ee
Goamhobala <yehbebe0112@gmail.com>
2024-09-07 12:25:59 +0200

    updated experiment script

--- speed-comparison-master/data/data.R
+++ speed-comparison-master/data/data.R
@@ -1,11 +1,10 @@
 # analysis.r
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
-mac_c <- read.csv("macM1/c0.1.csv")
-mac_java <- read.csv("macM1/java0.1.csv")
-
-log_mac_java <- log(mac_java)
-log_mac_c <- log(mac_c)
-
-sqrt_mac_c <- sqrt(mac_c)
-sqrt_mac_java <- sqrt(mac_java)
+ishango <- read.csv("ishango/pc2/pc2.csv")
+ishango.c <- ishango$c
+ishango.cpp <- ishango$cpp
+ishango.java <- ishango$java
+ishango.python <- ishango$python
+ishango.ruby <- ishango$ruby
+ishango.r <- ishango$r
--- speed-comparison-master/src/run_test.py
+++ speed-comparison-master/src/run_test.py
@@ -8,21 +8,24 @@ import random
 def set_up(languages):
     """Compile the code"""
     for lang in languages:
-        if lang["compile"] != "":
-            subprocess.run(lang["compile"].split())
+          print("setting up...")
+          lang["result"] = f"../results/{lang['name']}{rounds}.csv"
+          lang["total"] = 0
+          if lang["compile"] != "":
+              subprocess.run(lang["compile"].split())
+          if lang["warmup"] == True:
+              print("Warming up " + lang["name"]) 
+              for i in range(3):
+                  "warm up the code to get more accurate results"
+                  subprocess.run(f"{lang['run_env']}leibniz{lang['subscript']}".split())
+              print("Done warming up " + lang["name"])
+          
 
 def benchmark(rounds, languages=list):
     """Perform benchmarking for each language in the languages list"""
     rounds = int(rounds) * (10**-9)
     
     for lang in languages:
-        lang["result"] = f"../results/{lang['name']}{rounds}.csv"
-        if lang["warmup"] == True:
-            print("Warming up " + lang["name"]) 
-            for i in range(3):
-                "warm up the code to get more accurate results"
-                subprocess.run(f"{lang['run_env']}leibniz{lang['subscript']}".split())
-            print("Done warming up " + lang["name"])
         with open(lang["result"], mode= "a") as result:
             print("Testing " + lang["name"])
             start_time = time.perf_counter_ns()
@@ -31,63 +34,19 @@ def benchmark(rounds, languages=list):
             duration = end_time - start_time
             writer = csv.writer(result)
             writer.writerow([duration * (10**-6)])
-            
+            lang['total'] += duration
+     
+     
 
-def main(rounds):
+def run(languages):
     os.chdir("src")
+    with open("rounds.txt", mode="r") as f:
+     rounds = f.read()
     # some predefined languages info
-    c = {"name" : "c",
-         "compile": "gcc leibniz.c -o leibnizc -O3 ",
-         "warmup": True,
-         "run_env": "./",
-         "subscript": "c"}
-    cpp = {"name" : "cpp",
-         "compile": "g++ leibniz.c -o leibnizcpp -O3 -std=c++11",
-         "warmup": True,
-         "run_env": "./",
-         "subscript": "cpp"}
-    java = {"name" : "java",
-         "compile": "javac leibniz.java",
-         "warmup": True,
-         "run_env": "java ",
-         "subscript": ".java"}
-    javascript = {"name" : "javascript",
-         "compile": "",
-         "warmup": True,
-         "run_env": "node ",
-         "subscript": ".js"}
-    python = {"name" : "python",
-         "compile": "",
-         "warmup": False,
-         "run_env": "python3 ",
-         "subscript": ".py"}
-    r = {"name" : "r",
-         "compile": "",
-         "warmup": False,
-         "run_env": "Rscript ",
-         "subscript": ".r"}
-    php = {"name" : "php",
-         "compile": "",
-         "warmup": True,
-         "run_env": "php ",
-         "subscript": ".php"}
-    ruby = {"name" : "ruby",
-         "compile": "",
-         "warmup": False,
-         "run_env": "ruby ",
-         "subscript": ".rb"}
-    
-    languages = [c, cpp, java, ruby, python, r]
-    # randomising order
-    random.shuffle(languages)
     set_up(languages)
     benchmark(rounds, languages)
 
 
-with open("src/rounds.txt", mode="r") as f:
-     rounds = f.read()
-     
-main(rounds)
         
 
             
