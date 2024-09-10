60fe7005189dcea2f5b219ffb563d6368632da04
U-SCI-A-043\Student <SCI-A-043+Student@SCI-A-043.wf.uct.ac.za>
2024-09-10 10:48:27 +0200

    fixed auto_commit script

--- speed-comparison-master/auto_commit.py
+++ speed-comparison-master/auto_commit.py
@@ -6,7 +6,7 @@ def main(rounds, sample_size, num_samples):
 	subprocess.run(f"python3 run.py main {rounds} {sample_size} {num_samples}".split())
 	os.system("mv results/* data/scilabB/fifteen30")
 	subprocess.run(["git", "add", "."])
-	subprocess.run(["git", "commit", "-m", "Auto: test Push scilab data to github"])
+	subprocess.run(["git", "commit", "-m", "Auto: Push scilab data to github"])
 	subprocess.run(["git", "push", "git@github.com:Goamhobala/ED-Assignment.git"])
 
 
