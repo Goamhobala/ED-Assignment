e8731bd0ccfcf6b6687eb4c14e1c8e0123fadb4c
U-SCI-D-049\Student <SCI-D-049+Student@SCI-D-049.wf.uct.ac.za>
2024-09-11 14:37:01 +0200

    Auto: Push test's data to github

--- speed-comparison-master/auto_commit.py
+++ speed-comparison-master/auto_commit.py
@@ -2,11 +2,13 @@ import os
 import subprocess
 import sys
 
-def main(rounds, sample_size, num_samples):
+def main(rounds, sample_size, num_samples, machine):
 	subprocess.run(f"python3 run.py main {rounds} {sample_size} {num_samples}".split())
-	os.system("mv results/* data/Saurav/fifteen30")
+	subprocess.run(["git", "pull"])
+	os.system(f"mkdir data/{machine}/fifteen30")
+	os.system(f"mv results/* data/{machine}/fifteen30")
 	subprocess.run(["git", "add", "."])
-	subprocess.run(["git", "commit", "-m", "Auto: Push Saurav's data to github"])
+	subprocess.run(["git", "commit", "-m", f"Auto: Push {machine}'s data to github"])
 	subprocess.run(["git", "push", "git@github.com:Goamhobala/ED-Assignment.git"])
 
 
